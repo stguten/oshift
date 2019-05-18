e16bc5ed84837931a36bc471c7bf7fa92e07f6e8
Gutemberg <saddam.gutem@gmail.com>
2019-05-18 16:52:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,13 +19,13 @@ foreach($linhas as $l){
 	try{
 		$xxx = json_decode(file_get_contents("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/all/forecast/".$busStop[array_search($path[0]["type"],array_column($busStop,"name"))]["id"]."/".$path[0]["id"]."/1228"), TRUE);
 		for($i = 0; $i < count($xxx);$i++){
-			print_r("[".$xxx[$i]["codVehicle"]."]".$xxx[$i]["patternName"].": ".$xxx[$i]["arrivalTime"]." minutos\n");
+			print_r("[".$xxx[$i]["codVehicle"]."]".$xxx[$i]["patternName"].": ".$xxx[$i]["arrivalTime"]." minutos<br>");
 		}
 		$xxx2 = json_decode(file_get_contents("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/all/forecast/".$busStop[array_search($path[1]["type"],array_column($busStop,"name"))]["id"]."/".$path[1]["id"]."/1228"), TRUE);
 		for($i = 0; $i < count($xxx2);$i++){
-			print_r("[".$xxx2[$i]["codVehicle"]."]".$xxx2[$i]["patternName"].": ".$xxx2[$i]["arrivalTime"]." minutos\n");
+			print_r("[".$xxx2[$i]["codVehicle"]."]".$xxx2[$i]["patternName"].": ".$xxx2[$i]["arrivalTime"]." minutos<br>");
 		}
 	}catch(Exception $e){
-		print_r("[Erro]: ".$l["name"]." não existe ou apresentou erro.\n");
+		print_r("[Erro]: ".$l["name"]." não existe ou apresentou erro.<br>");
 	}
 }
