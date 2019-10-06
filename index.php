6edb1aef8de20c09aa37274b328f3e990aa148cf
Gutemberg <StGutem@users.noreply.github.com>
2019-10-05 21:13:31 -0300

    att

--- index.php
+++ index.php
@@ -13,18 +13,28 @@ $busStop = json_decode(file_get_contents("AllPoints.txt"), TRUE);
 
 foreach($linhas as $l){
 	
-	$path = json_decode(file_get_contents("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/pattern/".$l["id"]."/1228"), TRUE);
+	$path = json_decode(requisicao_handle("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/pattern/".$l["id"]."/1228"), TRUE);
 	
 	try{
-		$xxx = json_decode(file_get_contents("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/all/forecast/".$busStop[array_search($path[0]["type"],array_column($busStop,"name"))]["id"]."/".$path[0]["id"]."/1228"), TRUE);
+		$xxx = json_decode(requisicao_handle("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/all/forecast/".$busStop[array_search($path[0]["type"],array_column($busStop,"name"))]["id"]."/".$path[0]["id"]."/1228"), TRUE);
 		for($i = 0; $i < count($xxx);$i++){
 			print_r("[".$xxx[$i]["codVehicle"]."]".$xxx[$i]["patternName"].": ".$xxx[$i]["arrivalTime"]." minutos<br>");
 		}
-		$xxx2 = json_decode(file_get_contents("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/all/forecast/".$busStop[array_search($path[1]["type"],array_column($busStop,"name"))]["id"]."/".$path[1]["id"]."/1228"), TRUE);
+		$xxx2 = json_decode(requisicao_handle("http://zn5.m2mcontrol.com.br/api/forecast/lines/load/all/forecast/".$busStop[array_search($path[1]["type"],array_column($busStop,"name"))]["id"]."/".$path[1]["id"]."/1228"), TRUE);
 		for($i = 0; $i < count($xxx2);$i++){
 			print_r("[".$xxx2[$i]["codVehicle"]."]".$xxx2[$i]["patternName"].": ".$xxx2[$i]["arrivalTime"]." minutos<br>");
 		}
 	}catch(Exception $e){
 		print_r("[Erro]: ".$l["name"]." não existe ou apresentou erro.<br>");
 	}
+}
+
+function requisicao_handle($url)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$retorno = curl_exec($ch);
+	curl_close($ch);
+	return $retorno;
 }
