5406710c844cb316a78def09d666982933a6db11
Gutemberg <StGutem@users.noreply.github.com>
2019-10-07 14:20:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,11 @@ function requisicao_handle($url)
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$retorno = curl_exec($ch);
-	curl_close($ch);
-	return $retorno;
+	$info = curl_getinfo($ch);
+	if($info['CURLINFO_HTTP_CODE'] == 200){
+		curl_close($ch);
+		return curl_exec($ch);
+	}else{
+		throw new Exception('');
+	}
 }
